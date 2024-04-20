2c0b3ff1c773dfa84d0fccedf19f3d2fe7b9553f
worldchetan <worldchetan29@gmail.com>
2024-04-20 16:12:44 +0530

    change

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:5173/",
-  baseURL:
-    "https://6620149cbabda82b9a90f79d--marvelous-kulfi-a15ea3.netlify.app/",
+  baseURL: "http://localhost:5173/",
+  // baseURL:"https://6620149cbabda82b9a90f79d--marvelous-kulfi-a15ea3.netlify.app/",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export default instance;
